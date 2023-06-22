ea96b6b64e1589cfc28e8df47a3eb4966e150624
alexanderdomino <alexander.domino@gmail.com>
2023-06-22 21:46:04 +0200

    made create amounts show a list of existing amounts as well

--- src/Components/CreateAmount/CreateAmount.js
+++ src/Components/CreateAmount/CreateAmount.js
@@ -1,28 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './CreateAmount.css';
 
 function CreateAmount() {
     const [amountName, setAmountName] = useState('');
     const [amountml, setAmountML] = useState('');
+    const [amounts, setAmounts] = useState([]);
 
-    const handleSubmit = (event) => {
+    useEffect(() => {
+        fetchAmounts();
+    }, []);
+
+    const fetchAmounts = async () => {
+        try {
+            const response = await fetch('http://localhost:3001/amounts');
+            const data = await response.json();
+            setAmounts(data);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
         const newAmount = { amountName, amountml };
 
-        fetch('http://localhost:3001/amounts', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(newAmount),
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log('Success:', data);
-                setAmountName('');
-                setAmountML('');
-            })
-            .catch(error => console.error('Error:', error));
+        try {
+            const response = await fetch('http://localhost:3001/amounts', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(newAmount),
+            });
+            const data = await response.json();
+            console.log('Success:', data);
+            setAmountName('');
+            setAmountML('');
+            fetchAmounts();
+        } catch (error) {
+            console.error('Error:', error);
+        }
     };
 
     return (
@@ -44,6 +61,21 @@ function CreateAmount() {
                 />
                 <button type="submit">Save Amount</button>
             </form>
+
+            <div className="amounts-list">
+                <h2>Existing Amounts</h2>
+                {amounts.length === 0 ? (
+                    <p>No amounts found.</p>
+                ) : (
+                    <ul>
+                        {amounts.map(amount => (
+                            <li key={amount.id}>
+                                <p>{amount.amountName} - {amount.amountml} ml</p>
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </div>
         </div>
     );
 }
